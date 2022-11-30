335dfadd5503273338b55dd182a17a39c182a6ca
Dmitry <dmitrykrikotin@yandex.ru>
2022-11-30 03:46:43 +0300

    +Zadacha chetnosty

---
+++ DZ6_Chetnost/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Здравствуйте. Программа скажет четное ли число ");
+Console.Write("Введите число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+if (a%2==0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
