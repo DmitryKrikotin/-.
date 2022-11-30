f01ec859d084dd865f23cca205c5ab587c00ad36
Dmitry <dmitrykrikotin@yandex.ru>
2022-11-30 04:22:50 +0300

    +Zadacha8 posled chetnosty

---
+++ DZ8_posled_chetn/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Здравствуйте. Программа покажет все четные числа он 1 до N");
+Console.Write("Введите положительное число: ");
+int N=Convert.ToInt32(Console.ReadLine());
+int i=1;
+if (N>0)
+{
+    while (i<N+1)
+    if (i%2==0)
+    {
+    Console.WriteLine(i);
+    i++;
+    }
+    else
+    {  
+    i++;
+    }
+}
+else
+{
+    Console.WriteLine("Число отрицательное.");
+}
