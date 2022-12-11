6d3f7c8511c98f2783098861a0aac234e4cfde87
Dmitry <dmitrykrikotin@yandex.ru>
2022-12-11 20:27:36 +0300

    Задача 29

---
+++ DZ4_Zadacha29/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран.
+Console.WriteLine("Введите какое количество символов будет в массиве");
+int u=Convert.ToInt32(Console.ReadLine());
+int[] array=new int[u];
+for (int i=0; i<u;i++)
+{
+    array[i]=new Random().Next(0,101);
+    Console.Write(" "+ array[i]);
+}
