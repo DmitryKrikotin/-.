97419edb4f6957d3320738e81368d12b1d12a312
Dmitry <dmitrykrikotin@yandex.ru>
2022-12-11 19:38:55 +0300

    Задача семинара 25

---
+++ DZ4_Zadacha25/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите цикл, который принимает на вход два числа (А и В) и возводит число А в натуральную степень В.
+Console.WriteLine("Введите число, которое нужно возвести в степень");
+int A=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число, степень, в которую нужно возвести число");
+int B=Convert.ToInt32(Console.ReadLine());
+int stepen=1;
+for (int i=0; i<B; i++)
+ {
+   stepen=stepen*A;
+ }
+ Console.WriteLine("результат= "+stepen);
