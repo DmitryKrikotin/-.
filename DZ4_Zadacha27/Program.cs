28e8e7d1400c63460ca1f2adf7b5d22873615d4b
Dmitry <dmitrykrikotin@yandex.ru>
2022-12-11 20:07:01 +0300

    Задача 27

---
+++ DZ4_Zadacha27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе;
+Console.WriteLine("Введите число:");
+string A=Console.ReadLine();
+int i=A.Length;
+int B=Convert.ToInt32(A);
+int result=0;
+int l=0;
+for (int u=0;u<i;u++)
+{
+    l=B%10;
+    B=B/10;
+    result=result+l;
+    Console.WriteLine(result);
+}
+Console.WriteLine("Сумма равна=" + result);
