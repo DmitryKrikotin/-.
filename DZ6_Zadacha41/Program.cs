94daee705d984c3ac51bccaf598e0f4324870912
Dmitry <dmitrykrikotin@yandex.ru>
2022-12-14 20:22:12 +0300

    Задача 41

---
+++ DZ6_Zadacha41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите количество цифр массива:");
+int M=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа массива");
+int[] arr=new int[M];
+for (int i=0;i<M;i++)
+{
+    Console.WriteLine($"{i+1} элемент");
+    arr[i]=Convert.ToInt32(Console.ReadLine());
+}
+int k=0;
+for (int i=0;i<M;i++)
+{
+    if(arr[i]>0) k++;
+}
+Console.WriteLine($"Количество чисел >0 = {k}");
