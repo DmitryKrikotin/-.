65f0f2db6c00bafc44e3054a7e0b5bca5c5b8d96
Dmitry <dmitrykrikotin@yandex.ru>
2022-12-25 00:07:50 +0300

    Задача 56

---
+++ DZ8_Zadacha56/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+  
+Console.WriteLine("Задайте прямоугольный массив, получите номер строки с наименьшей суммой ");
+Console.WriteLine("Введите колличество строк массива");
+int m=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите колличество столбцов массива");
+int n=Convert.ToInt32(Console.ReadLine());
+if (m==n) Console.WriteLine("Массив не прямоугольный");
+int[,] array= new int[m,n];
+Random random = new Random();
+for (int i=0; i<m; i++)
+{
+    for (int j=0;j<n; j++)
+    {
+     array[i,j]=random.Next(1,10);
+     Console.Write($"{array[i, j],3} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+int maxSum=0;
+int sum=0;
+int indexMax=0;
+int secondIndexMax=0;
+int secondMaxSum=0;
+for(int i=0;i<m;i++)
+{
+  for(int j=0;j<n;j++)
+  
+  { {sum+=array[i,j];
+    Console.WriteLine(sum);}
+   if(maxSum<sum)
+    {
+      indexMax=i;
+      maxSum=sum;
+    }
+  }
+   if (maxSum==sum)
+    {
+      secondIndexMax=i;
+      secondMaxSum=sum;;
+    }
+  
+ Console.WriteLine();
+ sum=0;
+}
+Console.WriteLine($"Строка с максимальной суммой-{indexMax+1}  ");
+if ((secondMaxSum==maxSum)&&(indexMax!=secondIndexMax)) Console.WriteLine($"Строка вторая с той же суммой-{secondIndexMax+1}");
