9fa79e4119d98643813faff49fb8c0766bf3459c
Dmitry <dmitrykrikotin@yandex.ru>
2022-12-25 20:06:54 +0300

    Zadacha60

---
+++ DZ8_Zadacha60/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+Console.WriteLine("Программа по выводу трехмерного массива");
+int m=2;
+int n=2;
+int h=2;
+int[,,] array= new int[m,n,h]; 
+int[] temp = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+int  number;
+for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+int count = 0; 
+for (int i = 0;  i< array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(2); k++)
+      {
+        array[i, j,k] = temp[count];
+        Console.Write($"{array[i,j,k]} ({i},{j},{k}) ");
+        count++;
+      }
+    Console.WriteLine();
+    }
+  }
